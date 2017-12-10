3d8829af30ddd6303dcca984d545395715b15d34
Miguel Silva <msilva0897@gmail.com>
2017-12-09 22:54:25 -0700

    new test folders

---
+++ spec/factories/player_profiles.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :player_profile do
+    club "MyString"
+    height "MyString"
+    weight "MyString"
+    dominant_foot "MyString"
+    age "MyString"
+    primary_position "MyString"
+    secondary_position "MyString"
+  end
+end
---
+++ spec/factories/scout_profiles.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :scout_profile do
+    country "MyString"
+    reputation 1
+    availability false
+  end
+end
